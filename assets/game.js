5018da8a001f28afd4209c7e85b60229b3c32414
Steven Mazanec <SMazanec@proexam.org>
2018-05-07 15:53:50 -0400

    working version

---
+++ assets/game.js
@@ -0,0 +1,38 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lettersGuessed = [];
+var computerChoice =  letters[Math.floor(Math.random() * letters.length)];
+
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    if (lettersGuessed.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
+        lettersGuessed[lettersGuessed.length] = userGuess;
+        guessesLeft--;
+    }
+    
+    if (computerChoice === userGuess) {
+        wins++;
+        guessesLeft = 9;
+        lettersGuessed = [];
+        computerChoice =  letters[Math.floor(Math.random() * letters.length)];
+    }
+
+    if (guessesLeft === 0) {
+        losses++;
+        guessesLeft = 9;
+        lettersGuessed = [];
+        computerChoice =  letters[Math.floor(Math.random() * letters.length)];
+    }
+
+    var html = 
+    "<h1> The Psychic game </h1>" +
+    "<p> Guess what letter I'm thinking of </p>" +
+    "<p> Wins: " + wins + "</p>" +
+    "<p> Losses: " + losses + "</p>" +
+    "<p> Guesses Left: " + guessesLeft + "</p>" +
+    "<p> Guesses so far: " + lettersGuessed + "</p>";
+    document.querySelector("#game").innerHTML = html;
+}
